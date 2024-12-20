1082c6fa442a74218bcbc3429b573e4e859fb2ea
Braveheart3176 <rhey2476@gmail.com>
2024-12-20 15:07:05 -0800

    disable placeholder.ts

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,18 +1,18 @@
 // temp data if database or API is not yet set up
 
-const invoices = [
-  {
-    id: 1,
-    client: 'Angelo Yu',
-    amount: 100,
-    dueDate: '2022-01-01',
-    status: 'paid',
-  },
-  {
-    id: 2,
-    client: 'Jane Yu',
-    amount: 200,
-    dueDate: '2022-02-01',
-    status: 'unpaid',
-  }
-]
+// const invoices = [
+//   {
+//     id: 1,
+//     client: 'Angelo Yu',
+//     amount: 100,
+//     dueDate: '2022-01-01',
+//     status: 'paid',
+//   },
+//   {
+//     id: 2,
+//     client: 'Jane Yu',
+//     amount: 200,
+//     dueDate: '2022-02-01',
+//     status: 'unpaid',
+//   }
+// ]
